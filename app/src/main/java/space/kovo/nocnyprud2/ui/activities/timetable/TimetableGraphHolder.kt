9013164822898a95010a5082b601a131445f9b9b
kovo <kovo.mato@gmail.com>
2025-11-03 00:35:05 +0100

    FEAT - add time bar to timetable

--- app/src/main/java/space/kovo/nocnyprud2/ui/activities/timetable/TimetableGraphHolder.kt
+++ app/src/main/java/space/kovo/nocnyprud2/ui/activities/timetable/TimetableGraphHolder.kt
@@ -3,10 +3,12 @@ package space.kovo.nocnyprud2.ui.activities.timetable;
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.util.AttributeSet;
+import android.util.AttributeSet
 import android.widget.LinearLayout
-import kotlin.jvm.JvmOverloads;
 import androidx.core.graphics.toColorInt
+import java.time.LocalTime
+import kotlin.math.ceil
+
 
 class TimetableGraphHolder @JvmOverloads
 constructor(
@@ -16,6 +18,7 @@ constructor(
 
     val lineCount = 24
     private val hourColumn = Paint(Paint.ANTI_ALIAS_FLAG).apply { color = "#EEEEEE".toColorInt() }
+    private val hourPointerColumn = Paint(Paint.ANTI_ALIAS_FLAG).apply { color = "#ff0000".toColorInt() }
 
     init {
         setWillNotDraw(false) // Needed for layouts to receive onDraw() calls
@@ -23,6 +26,11 @@ constructor(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        drawHoursColums(canvas)
+        drawActualTimeBar(canvas)
+    }
+
+    fun drawHoursColums(canvas: Canvas) {
         val stepX = width / lineCount.toFloat()
         val stopY = height.toFloat()
         for (i in 0..lineCount) {
@@ -32,5 +40,18 @@ constructor(
             val x = i * stepX
             canvas.drawRect(x, 0f, x + stepX, stopY, hourColumn)
         }
+    }
+
+    fun drawActualTimeBar(canvas: Canvas) {
+        val stopY = height.toFloat()
+        val x = ceil(width * getDayFraction())
+        val timeBarWidth = (width / 24f / 8f).coerceAtLeast(2f)
+        canvas.drawRect(x, 0f, x + timeBarWidth, stopY, hourPointerColumn)
+    }
+
+    private fun getDayFraction(): Float {
+        val now = LocalTime.now()
+        val totalMinutes = now.hour * 60 + now.minute
+        return (totalMinutes / 1440f)
     }
 }
